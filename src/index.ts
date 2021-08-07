4d60ab29b788d19f8f40fd2e68de53397cbe8167
M.Odagawa <mus.29sup.guit@gmail.com>
2021-08-07 12:53:50 +0900

    Learn void and never types

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,20 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSample } from "./basic"
+// import { primitiveSample, notExistSample, anySample, unknownSample } from "./basic"
 
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
+
+// 04.関数の型定義
+import { logMessage } from "./function/basic"
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from "./function/parameters"
+
+logMessage("Hello TypeScript!")
+isUserSignedIn("ABC", "Milkybear")
+isUserSignedIn("DEF")
+isUserSignedIn2("ABC")
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function Parameters sample 5:', sum)
